966695bc3b3321588baa96835f5bbff6b3efab42
MShields1986 <matthew.shields.1986@gmail.com>
2024-03-21 16:44:32 +0000

    Updates for Noetic

--- iiwa_ros/scripts/ntp_verifier.py
+++ iiwa_ros/scripts/ntp_verifier.py
@@ -1,23 +1,24 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 import rospy
 from sensor_msgs.msg import JointState
 from iiwa_msgs.msg import CartesianPose
 
 rospy.init_node('ntp_verifier')
+rate = rospy.Rate(5)
 
 last_msg = None
+diff = None
 
 def ntplog(msg):
-	global last_msg
-	last_msg = msg
-
-def print_msg(msg):
-  if msg is not None:
+    global last_msg, diff
     diff = msg.poseStamped.header.stamp - rospy.Time.now()
-    print('The current time difference is: {} secs'.format(diff.to_sec()))
+    diff = round(diff.to_sec(), 6)
+    last_msg = msg
+
 
 sub = rospy.Subscriber('/iiwa/state/CartesianPose', CartesianPose, ntplog, queue_size=1)
 
 while not rospy.is_shutdown():
-	print_msg(last_msg)
+    rospy.loginfo(f'The current time difference is: {diff} secs')
+    rate.sleep()
